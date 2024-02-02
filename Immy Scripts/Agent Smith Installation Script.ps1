7891e05da81c32ef0a8c153b5e02242e6298bb1a
Woody <2997336+MWGMorningwood@users.noreply.github.com>
2024-02-02 17:20:10 -0500

    Create Agent Smith Installation Script.ps1

---
+++ Immy Scripts/Agent Smith Installation Script.ps1
@@ -0,0 +1,3 @@
+$LogFile = "C:\ProgramData\RewstRemoteAgent\$Rewst_Org_Id\logs\rewst_agent.log"
+$Arguments = "--config-url $Rewst_Webhook --config-secret $Rewst_Secret --org-id $Rewst_Org_Id"
+Start-ProcessWithLogTail $InstallerFile -ArgumentList $Arguments -LogFilePath $LogFile
